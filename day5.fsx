afff52216f36179bde1e39405d4c7ed1da291339
Sampo Siltanen <sampo.siltanen@solita.fi>
2020-12-05 10:25:11 +0200

    Day 5 task 2 first version

--- day5.fsx
+++ day5.fsx
@@ -1,17 +1,5 @@
 let input = System.IO.File.ReadAllLines "inputs/day5.txt"
 
-
-let row =
-    ("BBFFBBB" |> Seq.toList, 0, 127)
-    |> Array.unfold (fun (partitioning, lower, upper) ->
-        let mid = (lower + upper) / 2
-        match partitioning with
-        | [] -> Some (mid, (['X'], mid, mid))
-        | head :: tail when head = 'F' -> Some (mid, (tail, lower, mid))
-        | head :: tail when head = 'B' -> Some (mid+1, (tail, mid+1, upper))
-        | _ -> None)
-    |> Array.last
-
 let binarySpacePartitioning low up lowerBound upperBound (partitioning: string) =
     (partitioning |> Seq.toList, lowerBound, upperBound)
     |> Array.unfold (fun (partitioning, lower, upper) ->
@@ -22,3 +10,17 @@ let binarySpacePartitioning low up lowerBound upperBound (partitioning: string) 
         | head :: tail when head = up -> Some (mid+1, (tail, mid+1, upper))
         | _ -> None)
     |> Array.last
+
+let findSeat (partitioning: string) =
+    let row = binarySpacePartitioning 'F' 'B' 0 127 partitioning.[..6]
+    let col = binarySpacePartitioning 'L' 'R' 0 7 partitioning.[7..]
+    row, col
+
+let seatId (row,col) = row * 8 + col
+
+let seatIds = input |> Array.map (findSeat >> seatId) |> Array.sort
+let answer1 = Array.last seatIds
+let answer2 =
+    seatIds
+    |> Array.pairwise
+    |> Array.pick (fun (a,b) -> if b - a = 2 then Some ((b + a) / 2) else None)

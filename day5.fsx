7d1f9a97a0209048c93a3d4a6b49f7fc9bcd1d58
Sampo Siltanen <sampo.siltanen@solita.fi>
2020-12-05 13:41:47 +0200

    Refactor day 5

--- day5.fsx
+++ day5.fsx
@@ -2,29 +2,27 @@ let input = System.IO.File.ReadAllLines "inputs/day5.txt"
 
 #time "on"
 
-let binarySpacePartitioning low up lowerBound upperBound partitioning =
-    (Seq.toList partitioning, lowerBound, upperBound)
-    |> Array.unfold (fun (partitioning, lower, upper) ->
-        let mid = (lower + upper) / 2
-        match partitioning with
-        | [] -> Some (mid, (['X'], mid, mid))
-        | head :: tail when head = low -> Some (mid, (tail, lower, mid))
-        | head :: tail when head = up -> Some (mid+1, (tail, mid+1, upper))
-        | _ -> None)
-    |> Array.last
+let binarySpacePartitioning lowChar highChar sequence =
+    let middle a b = (a + b) / 2
+    sequence
+    |> Array.fold (fun (lo,hi) c ->
+        let mid = middle lo hi
+        if c = lowChar then (lo, mid)
+        elif c = highChar then (mid + 1, hi)
+        else failwith "Invalid sequence value") (0, (pown 2 sequence.Length) - 1)
+    |> fst
 
-let findSeat (partitioning: string) =
-    let row = binarySpacePartitioning 'F' 'B' 0 127 partitioning.[..6]
-    let col = binarySpacePartitioning 'L' 'R' 0 7 partitioning.[7..]
+let findSeat partitioning =
+    let rowData, colData = partitioning |> Seq.toArray |> Array.splitAt 7
+    let row = binarySpacePartitioning 'F' 'B' rowData
+    let col = binarySpacePartitioning 'L' 'R' colData
     row, col
 
 let seatId (row,col) = row * 8 + col
 
 let seatIds = input |> Array.map (findSeat >> seatId) |> Array.sort
+
 let answer1 = Array.last seatIds
-let answer2 =
-    seatIds
-    |> Array.pairwise
-    |> Array.pick (fun (a,b) -> if b - a = 2 then Some (a + 1) else None)
+let answer2 = seatIds |> Array.pairwise |> Array.pick (fun (a,b) -> if b - a = 2 then Some (a + 1) else None)
 
 #time "off"
